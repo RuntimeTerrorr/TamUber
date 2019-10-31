63c6fc286e13ec8a1c568dadf96890838ef4e0ff
mynameisosama <osama.aftab.1811@gmail.com>
2019-10-31 20:55:44 +0000

    fixed all cucumber tests

--- features/step_definitions/password_reset_steps.rb
+++ features/step_definitions/password_reset_steps.rb
@@ -1,6 +1,6 @@
 
 When(/^I should be on password reset page$/) do
-    expect(page).to have_content("Forgot password")
+    expect(page).to have_content("Forgot Password")
 end
 
 When(/^I'm on password reset page$/) do
