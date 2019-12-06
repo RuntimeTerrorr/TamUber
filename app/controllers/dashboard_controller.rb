562fe395f4ba821bc3b5bb028f897d43db8838a8
aman-jakkani <aman.jakkani@gmail.com>
2019-12-06 23:34:26 +0000

    try stub for vehicle id

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,10 +3,10 @@ class DashboardController < ApplicationController
   def get_stats 
     if VehicleStatus.exists?(vehicle_id: params[:vehicle_id])
       vehicle = VehicleStatus.find_by(vehicle_id: params[:vehicle_id])
-      @vehicle_stats = vehicle.update(params)
+      vehicle.update(params)
     else
       vehicle = VehicleStatus.new
-      @vehicle_stats = vehicle.update(params)
+      vehicle.update(params)
     end
   end
   
--- app/models/vehicle_status.rb
+++ app/models/vehicle_status.rb
@@ -1,10 +1,13 @@
 class VehicleStatus < ActiveRecord::Base
     
     def update?(params)
-        update_attribute(:vehicle_id, params["vehicle_id"])
-        update_attribute(:latitude, params["latitude"])
-        update_attribute(:longitude, params["longitude"])
-        update_attribute(:heading, params["heading"])
-        update_attribute(:occupancy, params["occupancy"])
+        update_attribute(:tire_pressure, params[:tire_pressure])
+        update_attribute(:battery, params[:battery])
+        update_attribute(:latitude, params[:latitude])
+        update_attribute(:longitude, params[:longitude])
+        update_attribute(:heading, params[:heading])
+        update_attribute(:occupancy, params[:occupancy])
+        @vehicle_stats = {"tire_pressure"=> params[:tire_pressure], "battery_level"=>params[:battery], "lidar_status"=> "None"}
     end
+
 end
