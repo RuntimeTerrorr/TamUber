364420029811b045361ee43759e21ca864ab733f
mynameisosama <osama.aftab.1811@gmail.com>
2019-12-08 06:40:13 +0000

    testing stats post request

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
   
   def post_stats
-    puts params
+    puts request.raw_post
+    puts params[:vehicle_id]
     if VehicleStatus.exists?(vehicle_id: params[:vehicle_id])
       vehicle = VehicleStatus.find_by(vehicle_id: params[:vehicle_id])
       vehicle.update(params)
